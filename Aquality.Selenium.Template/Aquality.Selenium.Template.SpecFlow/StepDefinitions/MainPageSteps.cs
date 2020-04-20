dbe4f80ac24ea635d804f82337598e82e528414a
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-04-20 14:35:11 +0300

    Reoganize step definitions

---
+++ Aquality.Selenium.Template/Aquality.Selenium.Template.SpecFlow/StepDefinitions/MainPageSteps.cs
@@ -0,0 +1,33 @@
+﻿using Aquality.Selenium.Browsers;
+using Aquality.Selenium.Template.Forms;
+using Aquality.Selenium.Template.Forms.Pages;
+using TechTalk.SpecFlow;
+
+namespace Aquality.Selenium.Template.SpecFlow.StepDefinitions
+{
+    [Binding]
+    public class MainPageSteps
+    {
+        private readonly MainPage mainPage;
+        private readonly TopBarMenu topBarMenu;
+
+        public MainPageSteps(MainPage mainPage, TopBarMenu topBarMenu)
+        {
+            this.mainPage = mainPage;
+            this.topBarMenu = topBarMenu;
+        }
+
+        [Given(@"Main page is opened")]
+        public void MainPageIsOpened()
+        {
+            AqualityServices.Browser.GoTo(Configuration.Configuration.StartUrl);
+        }
+        
+        [When(@"I open Contact us page")]
+        public void IOpenContactUsPage()
+        {
+            mainPage.AcceptCookies();
+            topBarMenu.OpenHeaderMenu(TopBarMenu.Item.ContactUs);
+        }
+    }
+}
