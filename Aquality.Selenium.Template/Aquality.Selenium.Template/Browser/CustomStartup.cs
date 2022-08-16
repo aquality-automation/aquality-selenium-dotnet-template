c5efa61169bf408da214ce532d84720f9685c89f
Aleksey2 Meleshko <a2.meleshko@a1qa.com>
2022-08-16 18:33:23 +0200

    [Feature] Log localized messages from Aquality Services to Allure report

---
+++ Aquality.Selenium.Template/Aquality.Selenium.Template/Browser/CustomStartup.cs
@@ -0,0 +1,20 @@
+﻿using Aquality.Selenium.Browsers;
+using Aquality.Selenium.Core.Applications;
+using Aquality.Selenium.Core.Configurations;
+using Aquality.Selenium.Core.Localization;
+using Aquality.Selenium.Template.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Aquality.Selenium.Template.Browser
+{
+    public class CustomStartup : BrowserStartup
+    {
+        public override IServiceCollection ConfigureServices(IServiceCollection services, Func<IServiceProvider, IApplication> applicationProvider, ISettingsFile settings = null)
+        {
+            base.ConfigureServices(services, applicationProvider, settings);
+            services.AddSingleton<ILocalizedLogger, AllureBasedLocalizedLogger>();
+            return services;
+        }
+    }
+}
---
+++ Aquality.Selenium.Template/Aquality.Selenium.Template/Logging/AllureExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Allure.Commons;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Aquality.Selenium.Template.Logging
+{
+    public static class AllureExtensions
+    {
+        /// <summary>
+        /// Wraps Action into AllureStep.
+        /// </summary>
+        public static void WrapInStep(this AllureLifecycle lifecycle, Action action, string stepName = "", [CallerMemberName] string callerName = "")
+        {
+
+            if (string.IsNullOrEmpty(stepName))
+            {
+                stepName = callerName;
+            }
+
+            var id = Guid.NewGuid().ToString();
+            var stepResult = new StepResult { name = stepName };
+            try
+            {
+                lifecycle.StartStep(id, stepResult);
+                action.Invoke();
+                lifecycle.StopStep(step => stepResult.status = Status.passed);
+            }
+            catch (Exception e)
+            {
+                lifecycle.StopStep(step =>
+                {
+                    step.statusDetails = new StatusDetails
+                    {
+                        message = e.Message,
+                        trace = e.StackTrace
+                    };
+                });
+                throw;
+            }
+        }
+    }
+}
