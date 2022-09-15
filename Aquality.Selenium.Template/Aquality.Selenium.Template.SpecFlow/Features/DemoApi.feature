5ade017ce239ec81267660b1678437a5af65295d
Aleksey2 Meleshko <a2.meleshko@a1qa.com>
2022-09-15 22:02:29 +0200

    Add steps to check JSON schema

--- Aquality.Selenium.Template/Aquality.Selenium.Template.SpecFlow/Features/DemoApi.feature
+++ Aquality.Selenium.Template/Aquality.Selenium.Template.SpecFlow/Features/DemoApi.feature
@@ -4,3 +4,9 @@ Feature: Demo API
 Scenario: GET user info
 	When I send GET '/users/aqualityAutomation' request to github and save the 'response'
 	Then the status code of the 'response' is '200'
+        And the 'response' matches json schema 'Users/UserResponse'
+
+Scenario: GET users info
+    When I send GET '/users' request to github and save the 'users response'
+    Then the status code of the 'users response' is '200'
+        And the 'users response' matches json schema 'Users/UsersResponse'
--- Aquality.Selenium.Template/Aquality.Selenium.Template.SpecFlow/StepDefinitions/API/Common/ResponseSteps.cs
+++ Aquality.Selenium.Template/Aquality.Selenium.Template.SpecFlow/StepDefinitions/API/Common/ResponseSteps.cs
@@ -1,5 +1,11 @@
-﻿using NUnit.Framework;
+﻿using Aquality.Selenium.Template.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using NUnit.Framework;
 using RestSharp;
+using System;
+using System.IO;
 using TechTalk.SpecFlow;
 
 namespace Aquality.Selenium.Template.SpecFlow.StepDefinitions.API.Common
@@ -11,6 +17,31 @@ namespace Aquality.Selenium.Template.SpecFlow.StepDefinitions.API.Common
         public static void StatusCodeOfResponseIs(RestResponse response, int statusCode)
         {
             Assert.That((int)response.StatusCode, Is.EqualTo(statusCode), "Status code should match to expected");
+        }
+
+        [Then(@"the '(.*response.*)' matches json schema '(.*)'")]
+        public void AssertResponseIsValidSchema(RestResponse response, string schemaName)
+        {
+            var schemaPath = GetSchemaPath(schemaName);
+            using (StreamReader file = File.OpenText(schemaPath))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JSchemaUrlResolver resolver = new JSchemaUrlResolver();
+
+                JSchema schema = JSchema.Load(reader, new JSchemaReaderSettings
+                {
+                    Resolver = resolver,
+                    BaseUri = new Uri(schemaPath)
+                });
+                var availablity = JToken.Parse(response.Content);
+                AttachmentHelper.AddAttachmentAsJson("json schema", schema);
+                Assert.That(availablity.IsValid(schema), Is.True, "Json schema should match to expected");
+            }
+        }
+
+        private static string GetSchemaPath(string fileName)
+        {
+            return Path.Combine(AppContext.BaseDirectory, "Resources", "JsonSchemas", $"{fileName}.json");
         }
     }
 }
