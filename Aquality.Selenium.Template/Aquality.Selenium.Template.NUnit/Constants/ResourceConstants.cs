7dc9af29467bddc306478abdeaff1ab55556cb95
Mikhail Pyshnik <m.pyshnik@a1qa.com>
2022-11-22 23:17:51 +0300

    Code was updated.Part 2.

--- Aquality.Selenium.Template/Aquality.Selenium.Template.NUnit/Constants/ResourceConstants.cs
+++ Aquality.Selenium.Template/Aquality.Selenium.Template.NUnit/Constants/ResourceConstants.cs
@@ -6,8 +6,8 @@ namespace Aquality.Selenium.Template.NUnit.Constants
     {
         public static string PathToContactUserWithInvalidEmail = @"Resources.TestData.ContactUserWithInvalidEmail.json";
         public static string PathToGetFullPageHeightJS = @"Resources.JavaScripts.GetFullPageHeight.js";
-        public static string ActualTestImage => Path.Combine("Resources", "ImagesForCustomImageComparator", "actual_test_image.jpg");
-        public static string ExpectedCorrectImage = Path.Combine("Resources", "ImagesForCustomImageComparator", "expected_correct_image.jpg");
-        public static string ExpectedIncorrectImage = Path.Combine("Resources", "ImagesForCustomImageComparator", "expected_incorrect_image.jpg");
+        public static string ActualTestImage => Path.Combine("Resources", "ImagesForCustomImageComparator", "ActualTestImage.jpg");
+        public static string ExpectedCorrectImage = Path.Combine("Resources", "ImagesForCustomImageComparator", "ExpectedCorrectImage.jpg");
+        public static string ExpectedIncorrectImage = Path.Combine("Resources", "ImagesForCustomImageComparator", "ExpectedIncorrectImage.jpg");
     }
 }
