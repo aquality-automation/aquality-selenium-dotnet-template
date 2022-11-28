00e2d049f352699c86665e4b2dd72ab25a05b25c
Mikhail Pyshnik <m.pyshnik@a1qa.com>
2022-11-28 16:08:06 +0300

    Add log for the linux debug

--- Aquality.Selenium.Template/Aquality.Selenium.Template.NUnit/Steps/FooterFormSteps.cs
+++ Aquality.Selenium.Template/Aquality.Selenium.Template.NUnit/Steps/FooterFormSteps.cs
@@ -1,12 +1,17 @@
-﻿using Aquality.Selenium.Template.CustomAttributes;
+﻿using Aquality.Selenium.Browsers;
+using Aquality.Selenium.Core.Configurations;
+using Aquality.Selenium.Core.Logging;
+using Aquality.Selenium.Template.CustomAttributes;
 using Aquality.Selenium.Template.Forms;
 using Aquality.Selenium.Template.NUnit.Extensions;
 using NUnit.Framework;
+using System.IO;
 
 namespace Aquality.Selenium.Template.NUnit.Steps
 {
     public class FooterFormSteps
     {
+        private static Logger Logger => Logger.Instance;
         private readonly FooterForm footerForm = new FooterForm();
 
         [LogStep(StepType.Assertion)]
@@ -35,6 +40,15 @@ namespace Aquality.Selenium.Template.NUnit.Steps
         [LogStep(StepType.Assertion)]
         public void CheckThatTheVisualElementsAreCorrect()
         {
+            var dir = AqualityServices.Get<IVisualizationConfiguration>().PathToDumps;
+            Logger.Info($"Directory - {dir}");
+            DirectoryInfo d = new DirectoryInfo(dir);
+            Logger.Info($" DirectoryInfo - {d.FullName}");
+            var temp = d.GetDirectories()[0].GetFiles();
+            foreach (var item in temp)
+            {
+                Logger.Info($"file info - [{item.FullName}]");
+            }
             var compareVisualElements = footerForm.Dump.Compare();
             Assert.AreEqual(0, compareVisualElements, "The footer form should contain the correct visual elements");
         }
